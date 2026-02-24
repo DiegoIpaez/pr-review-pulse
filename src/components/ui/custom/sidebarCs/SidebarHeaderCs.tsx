e333309914c9e34f3d002ea873a946f31d955fa2
DiegoIpaez <diegoipaez2000@gmail.com>
2026-02-24 01:55:39 -0300

    feat(ui/ux): a side menu is added

---
+++ src/components/ui/custom/sidebarCs/SidebarHeaderCs.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { SIDEBAR_TITLE_ROUTES } from '@/constants';
+import { usePathname } from 'next/navigation';
+import { SidebarTrigger } from '../../sidebar';
+
+export default function SidebarHeaderCs() {
+  const pathname = usePathname();
+  return (
+    <header className="flex h-14 items-center border-b px-4 lg:px-6">
+      <SidebarTrigger className="cursor-pointer" />
+      <h2 className="ml-4 text-base font-medium">
+        {SIDEBAR_TITLE_ROUTES?.[pathname]}
+      </h2>
+    </header>
+  );
+}
