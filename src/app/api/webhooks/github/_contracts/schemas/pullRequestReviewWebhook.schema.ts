3585d30373245a7000183bbe0877bf66a2372b32
DiegoIpaez <diegoipaez2000@gmail.com>
2026-02-25 10:53:26 -0300

    feat: the webhook is added to synchronize the gh review data to the database

---
+++ src/app/api/webhooks/github/_contracts/schemas/pullRequestReviewWebhook.schema.ts
@@ -0,0 +1,28 @@
+import { z as zod } from 'zod';
+import { GitHubReviewState } from '../types/index';
+
+export const pullRequestReviewWebhookSchema = zod.object({
+  review: zod.object({
+    state: zod.enum(GitHubReviewState),
+    body: zod.string().nullable(),
+    submitted_at: zod.string().datetime(),
+    user: zod.object({
+      login: zod.string().min(1),
+    }),
+  }),
+
+  pull_request: zod.object({
+    number: zod.number().int(),
+    created_at: zod.string().datetime(),
+    user: zod.object({
+      login: zod.string().min(1),
+    }),
+    head: zod.object({
+      ref: zod.string().min(1),
+    }),
+  }),
+
+  repository: zod.object({
+    name: zod.string().min(1),
+  }),
+});
