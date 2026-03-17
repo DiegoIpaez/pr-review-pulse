51cc952b47e9ed27dce1135e9245d45c1ac11ae3
DiegoIpaez <diegoipaez2000@gmail.com>
2026-03-17 20:18:54 -0300

    feat: username search is added

--- src/app/(menu)/users/page.tsx
+++ src/app/(menu)/users/page.tsx
@@ -1,9 +1,10 @@
 'use client';
-import { useState } from 'react';
+import { useState, type ChangeEvent } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import type { User } from '@/generated/prisma/client';
 import { PAGINATION } from '@/constants';
 import { fetchAllUsers } from '@/services/users.service';
+import { Input } from '@/components/ui/input';
 import DataTable from '@/components/ui/custom/data-table';
 import { userColumns } from './_components/users-columns';
 
@@ -11,10 +12,11 @@ export default function Home() {
   const [filters, setFilters] = useState({
     page: PAGINATION.DEFAULT_PAGE_NUMBER,
     limit: PAGINATION.DEFAULT_PAGE_SIZE,
+    search: '',
   });
 
   const { data, isLoading } = useQuery({
-    queryKey: ['users', filters],
+    queryKey: ['users', filters.page, filters.limit, filters.search],
     queryFn: () => fetchAllUsers(filters),
   });
 
@@ -22,13 +24,30 @@ export default function Home() {
     setFilters((prev) => ({ ...prev, page }));
   };
 
+  const handleSearchChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setFilters((prev) => ({
+      ...prev,
+      search: target?.value,
+      page: PAGINATION.DEFAULT_PAGE_NUMBER,
+    }));
+  };
+
   return (
-    <DataTable<User>
-      data={data}
-      isLoading={isLoading}
-      onPageChange={handlePageChange}
-      emptyMessage="Not found users."
-      columns={userColumns}
-    />
+    <div className="flex flex-col gap-4">
+      <Input
+        maxLength={50}
+        placeholder="Search by username..."
+        value={filters.search}
+        onChange={handleSearchChange}
+        className="max-w-sm"
+      />
+      <DataTable<User>
+        data={data}
+        isLoading={isLoading}
+        onPageChange={handlePageChange}
+        emptyMessage="Not found users."
+        columns={userColumns}
+      />
+    </div>
   );
 }
