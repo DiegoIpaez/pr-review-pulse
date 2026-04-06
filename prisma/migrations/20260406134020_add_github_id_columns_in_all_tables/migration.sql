fb3c6c7589afe26df08ff4e1598b419dc3f15734
DiegoIpaez <diegoipaez2000@gmail.com>
2026-04-06 10:41:11 -0300

    chore(db): the github id column is added to all gh tables

---
+++ prisma/migrations/20260406134020_add_github_id_columns_in_all_tables/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[github_id]` on the table `pull_request_reviews` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[github_id]` on the table `pull_requests` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[github_id]` on the table `repositories` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[github_id]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "pull_request_reviews" ADD COLUMN     "github_id" INTEGER;
+
+-- AlterTable
+ALTER TABLE "pull_requests" ADD COLUMN     "github_id" INTEGER;
+
+-- AlterTable
+ALTER TABLE "repositories" ADD COLUMN     "github_id" INTEGER;
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "github_id" INTEGER;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "pull_request_reviews_github_id_key" ON "pull_request_reviews"("github_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "pull_requests_github_id_key" ON "pull_requests"("github_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "repositories_github_id_key" ON "repositories"("github_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_github_id_key" ON "users"("github_id");
