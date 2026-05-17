ec42d9492ef37705d24b03a6ed1800d8226ea32e
DiegoIpaez <diegoipaez2000@gmail.com>
2026-05-17 01:26:44 -0300

    fix: the local api call has been corrected

--- src/constants/config.constant.ts
+++ src/constants/config.constant.ts
@@ -4,18 +4,12 @@ export enum NodeEnv {
   Test = 'test',
 }
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-const NEXT_AUTH = {
-  URL: process.env.NEXT_AUTH_URL || BASE_URL,
-  SECRET: process.env.NEXT_AUTH_SECRET || 'secret',
-};
-
 export const CONFIG = {
-  BASE_URL,
-  API_URL: `${BASE_URL}/api`,
   DATABASE_URL: process.env.DATABASE_URL || '',
   NODE_ENV: (process.env.NODE_ENV as NodeEnv) || NodeEnv.Development,
-  NEXT_AUTH,
+  NEXT_AUTH: {
+    SECRET: process.env.NEXTAUTH_SECRET || 'secret',
+  },
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || '',
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET || '',
   GITHUB_WEBHOOK_SECRET: process.env.GITHUB_WEBHOOK_SECRET || '',
