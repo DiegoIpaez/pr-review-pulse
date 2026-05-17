a3ec24fd2b56cc0f0b3e84620c89594e41d1bf1e
Diego Iván Paez <82846493+DiegoIpaez@users.noreply.github.com>
2026-05-17 01:33:36 -0300

    fix(no-ticket): deploy errors (#30)
    * chore: update version to 1.0.1
    
    * fix: language errors
    
    * fix: the local api call has been corrected
    
    * fix: the show all filtering has been corrected

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className={poppins.className}>
         <ThemeProvider
           attribute="class"
